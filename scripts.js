58dcb9738d16f64249d2bef497f174a4140333b3
Volodymyr Polishchuk <v.polishchuk@pollabs.io>
2023-05-31 18:39:29 +0300

    Add initial token loading

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
   document.querySelector('.scrimmage-widget-container .scrimmage-widget-fab')
     .addEventListener('click', (event) => {
       event.target.closest('.scrimmage-widget-container').classList.toggle('open');
     });
+  await setWidgetLink();
 });
+
+const setWidgetLink = async () => {
+  const backendUrl = 'https://us-central1-bright-practice-331514.cloudfunctions.net/requestGenerateAuthInfo';
+  const { token } = await fetch(backendUrl).then((response) => response.json());
+  const iframe = document.querySelector('.scrimmage-widget-container iframe');
+  iframe.src = `https://coinflip.apps.scrimmage.co/?token=${token}`;
+}
